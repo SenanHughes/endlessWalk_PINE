01b28499a3218d9bd254fe1ecf8d1e0f6ec77ac2
SenanHughes <senan_hughes@icloud.com>
2025-03-23 18:07:16 +0000

    .gitignore fix

--- endlessWalk/Intermediate/Build/Win64/x64/endlessWalkEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/senan/OneDrive/Documents/Masters in Computing in Comp Anim/finalProject/projectFiles/endlessWalk/Intermediate/Build/Win64/x64/endlessWalkEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- endlessWalk/Intermediate/Build/Win64/x64/endlessWalkEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/senan/OneDrive/Documents/Masters in Computing in Comp Anim/finalProject/projectFiles/endlessWalk/Intermediate/Build/Win64/x64/endlessWalkEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
